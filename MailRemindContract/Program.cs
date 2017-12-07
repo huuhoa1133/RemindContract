f5f786637e2058c65273fca6cc72320056b3b601
Hoa Huynh <huuhoa1133@gmail.com>
2017-12-07 23:51:05 +0700

    Add project files.

---
+++ MailRemindContract/Program.cs
@@ -0,0 +1,42 @@
+﻿using MailRemindContract.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailRemindContract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectDb = new ConnectDB();
+            var _db = connectDb.GetDB();
+            var schedule = new ScheduleRepository(_db);
+            var task = schedule.RemindContract();
+            Task.WaitAll(task);
+            Console.WriteLine("done");
+        }
+
+
+
+    }
+
+    public class dev
+    {
+        public void test()
+        {
+            var to = "hoahuynh@landsoft.com;hoahuynh@landsoft.com";
+            var tos = to.Split(';');
+            var x = FluentMail.CreateEmailFrom("landsofthcm@gmail.com")
+                    .BCC("abc@gm.com")
+                    .Subject("hoa huynh")
+                    .To(to.Split(';'))
+                    .Body("body")
+                    .SendAsync();
+            Task.WaitAll(x);
+        }
+    }
+
+}
---
+++ MailRemindContract/Repository/ConnectDB.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailRemindContract.Repository
+{
+    class ConnectDB
+    {
+        private static string ConnectionString { get; set; }
+        private static MongoUrl MongoUrl { get; set; }
+        private static MongoClient MongoClient { get; set; }
+        public static IMongoDatabase db { get; set; }
+
+        public IMongoDatabase GetDB()
+        {
+            ConnectionString = ConfigurationSettings.AppSettings["ConnectString"];
+            MongoUrl = new MongoUrl(ConnectionString);
+            MongoClient = new MongoClient(MongoUrl);
+            db = MongoClient.GetDatabase(MongoUrl.DatabaseName);
+
+            return db;
+        }
+    }
+}
